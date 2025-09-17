6c0360cbe763c8d636c638401af6b9c2dce2a25c
Víctor González Carro <victorglezcarro@Victors-MacBook-Pro.local>
2025-09-18 00:58:14 +0200

    configuracion vite arrreglada

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        { src: 'src/assets', dest: '' },       // dist/assets
+        { src: 'src/files', dest: '' },        // dist/files
+      ],
+    })
+  ],
 })
